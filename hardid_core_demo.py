c40c2d0e9978b2fe8977b5da6f8d3cad86d1e8f1
jillsoft-com <jillsoftx@gmail.com>
2026-05-19 23:56:12 +0800

    Update hardid_core_demo.py

--- hardid_core_demo.py
+++ hardid_core_demo.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
 # -------------------------------------------------------------------------
-# Copyright (c) 2026 jillsoft-com (Lijun Jiang). All rights reserved.
+# Copyright 2026 jillsoft-com (Lijun Jiang)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 # Project: HardID Protocol (Hardware-as-Identity)
 # Module: HardID-IM P2P Core Prototype (Alpha v1.0)
-# Description: Pure P2P decentralized identity handshake and encrypted chat.
-#              No central servers. Hardware is Identity.
-# Licensed under the Apache 2.0 License.
 # -------------------------------------------------------------------------
 
 import socket
